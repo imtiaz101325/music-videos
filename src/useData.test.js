ebdf51df1482b9cd13e4512f1af9e17af3f5737a
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2021-11-03 01:43:05 +0600

    Fixed test

--- src/useData.test.js
+++ src/useData.test.js
@@ -28,6 +28,7 @@ test('should load data', async () => {
         "title": "All In",
         "release_year": 2014,
         "genre_id": 14,
+        "genre": "Rap/Hip-Hop",
         "image_url": "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501437/images/app/w522_h292.jpg"
       }
     ]
