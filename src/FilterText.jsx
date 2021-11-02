4b468dd06e9237226a328078d29e46e2ee0b442d
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2021-11-03 01:37:05 +0600

    Added search icon

--- src/FilterText.jsx
+++ src/FilterText.jsx
@@ -1,21 +1,33 @@
 import { func } from "prop-types";
-import { Grid, TextField } from "@mui/material";
+import { Grid, InputAdornment, TextField } from "@mui/material";
+import { Search } from "@mui/icons-material";
 
 export default function FilterText({ setFilters }) {
   function handleChange(event) {
-    setFilters(filters => ({
+    setFilters((filters) => ({
       ...filters,
       search: event?.target?.value
-    }))
+    }));
   }
 
   return (
     <Grid item md={12}>
-      <TextField fullWidth label="Search" onChange={handleChange} />
+      <TextField
+        fullWidth
+        label="Search"
+        onChange={handleChange}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <Search />
+            </InputAdornment>
+          ),
+        }}
+      />
     </Grid>
   );
 }
 
 FilterText.propTypes = {
-  setFilters: func.isRequired
+  setFilters: func.isRequired,
 };
